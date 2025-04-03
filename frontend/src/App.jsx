535d320a40a9353894eb0ce7ae3a1b000cdc13ee
hvchaturvedi <hvchaturvedi1@gmail.com>
2025-04-03 19:50:28 +0530

    Initial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/signup";
+import { Signin } from "./pages/signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App
